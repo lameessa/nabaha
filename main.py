10cfc97480a7ba9a639f5d8e2f213a735eb1ad99
lameessa <444201177@student.ksu.edu.sa>
2025-08-01 19:38:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+import whisper
 import json
+import datetime
 import joblib
 from sentence_transformers import SentenceTransformer
-import numpy as np
+import soundfile as sf
 import traceback
+import numpy as np
 
-# Load models
+# Load models once
+model = whisper.load_model("tiny")
 encoder = SentenceTransformer("asafaya/bert-base-arabic")
 clf = joblib.load("vishing_classifier.pkl")
 
-def process_text_message(text):
+def process_audio_file(file_path, label_weights=None):
     try:
+        # Load audio
+        audio_data, sr = sf.read(file_path, dtype='float32')
+        if sr != 16000:
+            raise ValueError("Sample rate must be 16kHz")
+
+        # Transcribe
+        result = model.transcribe(file_path, language='ar')
+        text = result["text"].strip()
+
+        if text.strip() == "":
+            return {"text": "", "prediction": "No speech detected."}
+
+        # Encode transcript
         features = encoder.encode([text])
-        preds = clf.predict(features)[0]
-        probs = clf.predict_proba(features)
-        max_index = int(np.argmax(preds))
-        max_prob = float(np.max(probs[max_index]))
+
+        # Predict
+        preds = clf.predict(features)[0]              # Array of 0/1
+        probs = clf.predict_proba(features)           # List of arrays
+        max_index = int(np.argmax(preds))             # Which class got "1"
+        max_prob = float(np.max(probs[max_index]))    # Max confidence
 
         labels = [
             "رمز تحقق", "بنك", "تهديد", "رقم بطاقة",
